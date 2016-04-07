142bc236302e2c771e7f5ddee1ed9be037fd8a23
daviherr1 <David.Herron@Astontech.com>
2016-04-07 14:40:54 -0700

    test commit

--- src/main/java/com/astontech/hr/repositories/ElementRepository.java
+++ src/main/java/com/astontech/hr/repositories/ElementRepository.java
@@ -3,8 +3,15 @@ package com.astontech.hr.repositories;
 import com.astontech.hr.domain.Element;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 /**
  * Created by daviherr1 on 4/7/2016.
  */
 public interface ElementRepository extends CrudRepository<Element, Integer>{
+
+    // This will generate the required code from the structure of the method name
+    Element findByElementName (String elementName);
+
+    List<Element> findAllByElementName(String elementName);
 }
--- src/test/java/repositories/ElementRepositoryTest.java
+++ src/test/java/repositories/ElementRepositoryTest.java
@@ -10,6 +10,9 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
@@ -48,6 +51,51 @@ public class ElementRepositoryTest {
         assertEquals(updatedElement.getElementName(), "Email");
     }
 
-    @Testdfg
+    @Test
+    public void testSaveElementList() {
+        List<Element> elementList = new ArrayList<>();
 
+        elementList.add(new Element("Phone"));
+        elementList.add(new Element("Email"));
+        elementList.add(new Element("Laptop"));
+        elementList.add(new Element("PayRate"));
+
+        elementRepository.save(elementList);
+
+        Iterable<Element> fetchedElementList = elementRepository.findAll();
+
+        int count = 0;
+        for (Element element : fetchedElementList) {
+            count++;
+        }
+
+//        assertEquals(4, count);
+    }
+
+
+
+    @Test
+    public void testFindByName() {
+        Element element = new Element("FindTestSingle");
+        elementRepository.save(element);
+
+        Element foundByNameElement = elementRepository.findByElementName("FindTestSingle");
+
+        assertEquals(element.getId(), foundByNameElement.getId());
+    }
+
+    @Test
+    public void testFindAllByName() {
+        Element element1 = new Element("FindTest");
+        elementRepository.save(element1);
+        Element element2 = new Element("FindTest");
+        elementRepository.save(element2);
+        Element element3 = new Element("FindTest");
+        elementRepository.save(element3);
+
+        List<Element> foundAllByNameElement = elementRepository.findAllByElementName("FindTest");
+
+        assertEquals(3, foundAllByNameElement.size());
+
+    }
 }
